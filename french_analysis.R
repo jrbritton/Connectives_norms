7f9ea7bd390b455c2336f002ac7ebcb6391a6734
James Britton <70253337+Hebereke2@users.noreply.github.com>
2023-06-28 11:51:20 +0800

    Add survey files

---
+++ french_analysis.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(lme4)
+library(postHoc)
+library(DescTools)
+library(agricolae)
+library(RColorBrewer)
+library(sjPlot)
+library(ordinal)
+library(emmeans)
+library(stringr)
+library(openxlsx)
+
+setwd("C:\\Users\\herts\\OneDrive\\Desktop\\Manu\\data")
+
+frenchData <- read.csv("french_data.csv")
+frenchSentConds <- read.csv("frenchConditions.csv", encoding = "French")
+frenchSentConds <- frenchSentConds[,c(-2:-4)]
+
+table(frenchData$Age)
+
+frenchData1a <- frenchData[c(1:5), c(1,4,5,7,10:89)]
+frenchData1b <- frenchData[c(6:11), c(1,4,5,7,100:166)]
+frenchData2a <- frenchData[c(12:16), c(1,4,5,7,167:246)]
+frenchData2b <- frenchData[c(17:21), c(1,4,5,7,247:323)]
+frenchData3a <- frenchData[c(22:26), c(1,4,5,7,324:403)]
+frenchData3b <- frenchData[c(27:32), c(1,4,5,7,404:480)]
+frenchData4a <- frenchData[c(33:38), c(1,4,5,7,481:560)]
+frenchData4b <- frenchData[c(39:44), c(1,4,5,7,561:637)]
+
+# Melt data to long format
+
+frenchData1a <- reshape2::melt(frenchData1a, id.vars = c(1:4))
+colnames(frenchData1a)[colnames(frenchData1a)=="variable"] = "Item"
+colnames(frenchData1a)[colnames(frenchData1a)=="value"] = "Score"
+
+frenchData1b <- reshape2::melt(frenchData1b, id.vars = c(1:4))
+colnames(frenchData1b)[colnames(frenchData1b)=="variable"] = "Item"
+colnames(frenchData1b)[colnames(frenchData1b)=="value"] = "Score"
+
+frenchData2a <- reshape2::melt(frenchData2a, id.vars = c(1:4))
+colnames(frenchData2a)[colnames(frenchData2a)=="variable"] = "Item"
+colnames(frenchData2a)[colnames(frenchData2a)=="value"] = "Score"
+
+frenchData2b <- reshape2::melt(frenchData2b, id.vars = c(1:4))
+colnames(frenchData2b)[colnames(frenchData2b)=="variable"] = "Item"
+colnames(frenchData2b)[colnames(frenchData2b)=="value"] = "Score"
+
+frenchData3a <- reshape2::melt(frenchData3a, id.vars = c(1:4))
+colnames(frenchData3a)[colnames(frenchData3a)=="variable"] = "Item"
+colnames(frenchData3a)[colnames(frenchData3a)=="value"] = "Score"
+
+frenchData3b <- reshape2::melt(frenchData3b, id.vars = c(1:4))
+colnames(frenchData3b)[colnames(frenchData3b)=="variable"] = "Item"
+colnames(frenchData3b)[colnames(frenchData3b)=="value"] = "Score"
+
+frenchData4a <- reshape2::melt(frenchData4a, id.vars = c(1:4))
+colnames(frenchData4a)[colnames(frenchData4a)=="variable"] = "Item"
+colnames(frenchData4a)[colnames(frenchData4a)=="value"] = "Score"
+
+frenchData4b <- reshape2::melt(frenchData4b, id.vars = c(1:4))
+colnames(frenchData4b)[colnames(frenchData4b)=="variable"] = "Item"
+colnames(frenchData4b)[colnames(frenchData4b)=="value"] = "Score"
+
+# Rbind and merge all data 
+
+allDataFre <- rbind(frenchData1a,frenchData1b,frenchData2a,frenchData2b,
+                 frenchData3a,frenchData3b,frenchData4a,frenchData4b)
+
+allDataFre <- merge(allDataFre,frenchSentConds, by.x = "Item", by.y = "SOS_ID")
+#View(frenchSentConds)
+# Means by Sentence Condition
+
+frenchCondNorms <- aggregate(Score~CONDITION, data=allDataFre,mean); 
+names(frenchCondNorms) = c("CONDITION","Mean") 
+
+frenchCondSD <- aggregate(Score~CONDITION, data=allDataFre,sd); 
+names(frenchCondSD)= c("CONDITION","SD") 
+
+frenchCondNorms <- merge(frenchCondNorms,frenchCondSD, by = "CONDITION")
+View(frenchCondNorms)
+
+# Plot for distributions
+
+ggplot(allDataFre, aes(x=Score)) +
+  geom_histogram(binwidth=.5, colour="black", fill="steelblue") +
+  geom_vline(aes(xintercept=mean(Score, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1)
+
+scoreBoxFre <- ggplot(allDataFre, aes(x = CONDITION, y = Score, fill = CONDITION)) +
+  geom_boxplot() +
+  ggtitle("Sentence Conditions")
+scoreBoxFre
+
+# Conditions Model
+
+allDataFre$Score <- as.factor(allDataFre$Score)
+allDataFre$Part <- as.factor(allDataFre$Part)
+
+
+clmmFrench <- clmm(Score~CONDITION + TARGET_WORD + (1|Part), data = allDataFre) 
+summary(clmmFrench) 
+
+emmeans(clmmFrench,pairwise ~ CONDITION | Score, mode = "prob")
+
---
+++ italian_analysis.R
@@ -0,0 +1,286 @@
+library(tidyverse)
+library(lme4)
+library(postHoc)
+library(DescTools)
+library(agricolae)
+library(RColorBrewer)
+library(sjPlot)
+library(ordinal)
+library(emmeans)
+library(openxlsx)
+
+setwd("C:\\Users\\herts\\OneDrive\\Desktop\\Manu\\connectives\\survey_data")
+
+#Sys.setlocale(category="LC_ALL", locale = "English_United States.1252")
+View(italianData)
+italianData <- read.csv("italianData.csv")
+italianConds <- read.csv("italianConditions.csv", encoding = "Italian")
+italianConds <- italianConds[,c(-2:-4)]
+#View(italianConds)
+# A pie chart of age distribution
+
+# ageSum <- data.frame(table(italianData$Age))
+# pieColours <- c("maroon", "deepskyblue", "lightgreen", "yellow3", "purple",
+#                 "brown2", "orange", "pink", "turquoise", "green4", "orange4")
+# 
+# agePie <- pie(ageSum$Freq, labels = ageSum$Var1, col = pieColours, border = FALSE)
+
+
+# Change the values here later when more data is collected 
+
+italianData1a <- italianData[c(1:5), c(2,4,5,7,8:87)]
+italianData1b <- italianData[c(6:10), c(2,4,5,7,88:164)]
+italianData2a <- italianData[c(11:15), c(2,4,5,7,165:244)]
+italianData2b <- italianData[c(16:20), c(2,4,5,7,245:321)]
+italianData3a <- italianData[c(21:25), c(2,4,5,7,322:401)]
+italianData3b <- italianData[c(26:30), c(2,4,5,7,402:478)]
+italianData4a <- italianData[c(31:35), c(2,4,5,7,479:558)]
+italianData4b <- italianData[c(36:40), c(2,4,5,7,559:635)]
+
+# Melt data to long format
+
+italianData1a <- reshape2::melt(italianData1a, id.vars = c(1:4))
+colnames(italianData1a)[colnames(italianData1a)=="variable"] = "Item"
+colnames(italianData1a)[colnames(italianData1a)=="value"] = "Score"
+
+italianData1b <- reshape2::melt(italianData1b, id.vars = c(1:4))
+colnames(italianData1b)[colnames(italianData1b)=="variable"] = "Item"
+colnames(italianData1b)[colnames(italianData1b)=="value"] = "Score"
+
+italianData2a <- reshape2::melt(italianData2a, id.vars = c(1:4))
+colnames(italianData2a)[colnames(italianData2a)=="variable"] = "Item"
+colnames(italianData2a)[colnames(italianData2a)=="value"] = "Score"
+
+italianData2b <- reshape2::melt(italianData2b, id.vars = c(1:4))
+colnames(italianData2b)[colnames(italianData2b)=="variable"] = "Item"
+colnames(italianData2b)[colnames(italianData2b)=="value"] = "Score"
+
+italianData3a <- reshape2::melt(italianData3a, id.vars = c(1:4))
+colnames(italianData3a)[colnames(italianData3a)=="variable"] = "Item"
+colnames(italianData3a)[colnames(italianData3a)=="value"] = "Score"
+
+italianData3b <- reshape2::melt(italianData3b, id.vars = c(1:4))
+colnames(italianData3b)[colnames(italianData3b)=="variable"] = "Item"
+colnames(italianData3b)[colnames(italianData3b)=="value"] = "Score"
+
+italianData4a <- reshape2::melt(italianData4a, id.vars = c(1:4))
+colnames(italianData4a)[colnames(italianData4a)=="variable"] = "Item"
+colnames(italianData4a)[colnames(italianData4a)=="value"] = "Score"
+
+italianData4b <- reshape2::melt(italianData4b, id.vars = c(1:4))
+colnames(italianData4b)[colnames(italianData4b)=="variable"] = "Item"
+colnames(italianData4b)[colnames(italianData4b)=="value"] = "Score"
+
+# Rbind and merge all data 
+
+allData <- rbind(italianData1a,italianData1b,italianData2a,italianData2b,
+                 italianData3a,italianData3b,italianData4a,italianData4b)
+
+#View(allData)
+
+allData <- merge(allData,italianConds, by.x = "Item", by.y = "SOS_ID")
+
+# Means by Sentence Condition
+
+italianCondNorms <- aggregate(Score~CONDITION, data=allData,mean); 
+names(italianCondNorms) = c("CONDITION","Mean") 
+
+italianCondSD <- aggregate(Score~CONDITION, data=allData,sd); 
+names(italianCondSD)= c("CONDITION","SD") 
+
+italianCondNorms <- merge(italianCondNorms,italianCondSD, by = "CONDITION")
+#View(italianCondNorms)
+
+#write.csv(italianCondNorms,"C:/Users/herts/OneDrive/Desktop/condNorms.csv", fileEncoding = "UTF-8")
+
+# Means by Word
+
+italianWordNorms <- aggregate(Score~TARGET_WORD, data=allData,mean); 
+names(italianWordNorms) = c("TARGET_WORD","Mean") 
+
+italianWordSD <- aggregate(Score~TARGET_WORD, data=allData,sd); 
+names(italianWordSD)= c("TARGET_WORD","SD") 
+
+italianWordNorms <- merge(italianWordNorms,italianWordSD, by = "TARGET_WORD")
+#View(italianWordNorms)
+
+condsWords <- italianConds[,c(2,3)]
+
+# sc1 <- subset(allData, CONDITION == "PLAUS_NOCONN")
+# sc2 <- subset(allData, CONDITION == "IMPLAUS_NOCONN")
+# sc3 <- subset(allData, CONDITION == "PLAUS_CONN")
+# sc4 <- subset(allData, CONDITION == "IMPLAUS_CONN")
+
+# Add a column for all the participant data
+
+italianData$all <- "Parts"
+
+# Create a vector of 'a' and 'b' to add to italianData for each quest type
+
+questAB <- c(rep('a',5),rep('b',5),rep('a',5),rep('b',5),rep('a',5),
+            rep('b',5),rep('a',5),rep('b',5))
+
+italianData$questAB <- questAB
+
+# Plot for distributions
+#view(allData)
+ggplot(allData, aes(x=Score)) + geom_histogram(binwidth=.5)
+# qplot(dat$rating, binwidth=.5)
+
+# Draw with black outline, white fill
+ggplot(allData, aes(x=Score)) +
+  geom_histogram(binwidth=.5, colour="black", fill="steelblue") +
+  geom_density(aes(x=Score)) +
+  geom_vline(aes(xintercept=mean(Score, na.rm=T)),   # Ignore NA values for mean
+           color="red", linetype="dashed", size=1)
+
+# Density curve
+ggplot(allData, aes(x=Score)) + geom_density()
+
+timeBox <- ggplot(italianData, aes(x = Quest, y = Time, fill = Quest)) +
+  geom_boxplot() +
+  ggtitle("Time to complete questionnaire")
+timeBox
+
+#timeNorms <- aggregate(Time~Quest, data=italianData,mean); 
+#names(timeNorms) = c("Quest","Mean") 
+
+#write.csv(timeNorms,"C:/Users/herts/OneDrive/Desktop/timeNorms.csv", fileEncoding = "UTF-8")
+
+questTimeBox <- ggplot(italianData, aes(x = questAB, y = Time, fill = questAB)) +
+  geom_boxplot() +
+  ggtitle("Time to complete questionnaire")
+questTimeBox
+
+scoreBox <- ggplot(allData, aes(x = CONDITION, y = Score, fill = CONDITION)) +
+  geom_boxplot() +
+  ggtitle("Sentence Conditions")
+scoreBox
+
+
+# Conditions model
+# 
+# condModel <- aov(Score ~ CONDITION, data = allData)
+# summary(condModel)
+# 
+# condTukeyTest <- TukeyHSD(condModel)
+# condTukeyTest
+# 
+# condTukeyTest2 <- HSD.test(condModel, "CONDITION", group = FALSE)
+# condTukeyTest2 
+# 
+# par(mfrow=c(2,2))
+# plot(condModel)
+# par(mfrow=c(1,1))
+# 
+# tukeyPlot <- TukeyHSD(condModel)
+# plot(tukeyPlot, las = 1)
+# 
+# png(filename="tukeplot.png", width=900, bg="white")
+# par(mar=c(5,6,4,1)+.1)
+# 
+# tukeyPlot <- TukeyHSD(condModel)
+# axis(2, at=1:3, las=1, cex.axis=1.3)
+# dev.off()
+
+# High and low scores for Implaus_NoConn and Plaus_NoConn
+
+impNoConnHi <- subset(allData, CONDITION == "IMPLAUS_NOCONN" & Score >= 4)
+
+plausNoConnLo <- subset(allData, CONDITION == "PLAUS_NOCONN" & Score <= 4)
+
+italianAllRotas <- read.csv("italianAllRotas.csv")
+italianAllRotas <- subset(italianAllRotas, select = c(1:5))
+
+impNoConnHi <- merge(italianAllRotas,impNoConnHi, by.x = "SOS_ID", 
+                     by.y = "Item")
+
+plausNoConnLo <- merge(italianAllRotas,plausNoConnLo, by.x = "SOS_ID", 
+                       by.y = "Item")
+
+#write.csv(plausNoConnLo,"C:/Users/herts/OneDrive/Desktop/impNoConn_low.csv", fileEncoding = "UTF-8")
+
+# allData[allData == "SC1"] <- 1
+# allData[allData == "SC2"] <- 2
+# allData[allData == "SC3"] <- 3
+# allData[allData == "SC4"] <- 4
+
+# allData$Score <- as.numeric(allData$Score)
+# allData$SENT_COND <- as.numeric(allData$SENT_COND)
+# Binomial model
+
+# allData$Score[allData$Score == 1] <- "Very Incoherent"
+# allData$Score[allData$Score == 2] <- "Incoherent"
+# allData$Score[allData$Score == 3] <- "Somewhat Incoherent"
+# allData$Score[allData$Score == 4] <- "Neutral"
+# allData$Score[allData$Score == 5] <- "Somewhat Coherent"
+# allData$Score[allData$Score == 6] <- "Coherent"
+# allData$Score[allData$Score == 7] <- "Very Coherent"
+
+allData$Score <- as.factor(allData$Score)
+allData$Part <- as.factor(allData$Part)
+
+itaclmmModel <- clmm(Score~CONDITION + Gender + (1|Part), data = allData) 
+summary(itaclmmModel) 
+
+emmeans(itaclmmModel,pairwise ~ CONDITION | Score, mode = "prob")
+
+
+# Calculate clause lengths
+
+sent_len = c()  # Empty vector
+
+for (var in italianAllRotas$SENTENCE){
+  var = length(strsplit(var, " ")[[1]])
+  it = var     # 'it' is the value of var after one iteration
+  sent_len = c(sent_len, it) # add the value of it to empty vector
+}
+
+sent_len = data.frame(sent_len)  # make for loop output into a data frame
+
+sent_lengths <- data.frame(table(sent_len))
+
+colnames(sent_lengths) <- c("WordCount", "SentNum")
+italianAllRotas <- cbind(italianAllRotas,sent_len)
+
+#write.xlsx(sent_lengths,"C:/Users/herts/OneDrive/Desktop/sent_lengths.xlsx", 
+#           colNames = TRUE)
+#write.xlsx(italianAllRotas,"C:/Users/herts/OneDrive/Desktop/allItalianRotas.xlsx", 
+#           colNames = TRUE)
+
+subRotas1 <- subset(italianAllRotas, CONDITION == "PLAUS_NOCONN" & 
+                      sent_len == 22)
+subRotas2 <- subset(italianAllRotas, CONDITION == "IMPLAUS_NOCONN" & 
+                      sent_len == 22)
+subRotas3 <- subset(italianAllRotas, CONDITION == "PLAUS_CONN" & 
+                      sent_len == 22)
+subRotas4 <- subset(italianAllRotas, CONDITION == "IMPLAUS_CONN" & 
+                      sent_len == 22)
+
+words_22 <- rbind(subRotas1,subRotas2,subRotas3,subRotas4)
+View(words_22)
+table(words_22$CONDITION)
+
+clauses2 <- read.csv("clauses2.csv")
+table(clauses2$CONDITION)
+
+subRotas1 <- subset(italianAllRotas, CONDITION == "PLAUS_NOCONN" & 
+                      sent_len == 23)
+subRotas2 <- subset(italianAllRotas, CONDITION == "IMPLAUS_NOCONN" & 
+                      sent_len == 23)
+subRotas3 <- subset(italianAllRotas, CONDITION == "PLAUS_CONN" & 
+                      sent_len == 23)
+subRotas4 <- subset(italianAllRotas, CONDITION == "IMPLAUS_CONN" & 
+                      sent_len == 23)
+
+words_23 <- rbind(subRotas1,subRotas2,subRotas3,subRotas4)
+table(words_23$CONDITION)
+
+
+write.xlsx(words_22,"C:/Users/herts/OneDrive/Desktop/words_22.xlsx", 
+           colNames = TRUE)
+write.xlsx(words_23,"C:/Users/herts/OneDrive/Desktop/words_23.xlsx", 
+           colNames = TRUE)
+
+write.csv(words_22,"C:/Users/herts/OneDrive/Desktop/words_22.csv", 
+          fileEncoding = "UTF-8")
