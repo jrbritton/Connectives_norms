4a678e257bd96d837d601cff3131812e7565c294
James Britton <70253337+Hebereke2@users.noreply.github.com>
2023-06-29 15:18:01 +0800

    Concessive connective analysis Added code for cleaning concessive data. Added box and distribution plots for the concessive data.

--- italian_analysis.R
+++ italian_analysis.R
@@ -39,7 +39,7 @@ italianData3a <- italianData[c(21:25), c(2,4,5,7,322:401,636)]
 italianData3b <- italianData[c(26:30), c(2,4,5,7,402:478,636)]
 italianData4a <- italianData[c(31:35), c(2,4,5,7,479:558,636)]
 italianData4b <- italianData[c(36:40), c(2,4,5,7,559:635,636)]
-View(italianData)
+#View(italianData)
 # Melt data to long format
 
 italianData1a <- reshape2::melt(italianData1a, id.vars = c(1:4,85))
@@ -79,12 +79,12 @@ colnames(italianData4b)[colnames(italianData4b)=="value"] = "Score"
 allData <- rbind(italianData1a,italianData1b,italianData2a,italianData2b,
                  italianData3a,italianData3b,italianData4a,italianData4b)
 
-View(allData)
+#View(allData)
 
 allData <- merge(allData,italianConds, by.x = "Item", by.y = "SOS_ID")
 
 # Means by Sentence Condition
-
+View(allData)
 italianCondNorms <- aggregate(Score~CONDITION, data=allData,mean); 
 names(italianCondNorms) = c("CONDITION","Mean") 
 
@@ -135,7 +135,8 @@ ggplot(allData, aes(x=Score)) +
   geom_histogram(binwidth=.5, colour="black", fill="steelblue") +
   geom_density(aes(x=Score)) +
   geom_vline(aes(xintercept=mean(Score, na.rm=T)),   # Ignore NA values for mean
-           color="red", linetype="dashed", size=1)
+           color="red", linetype="dashed", size=1) +
+  ggtitle("Distribution: Adversative ('Tuttavia')")
 
 # Density curve
 ggplot(allData, aes(x=Score)) + geom_density()
@@ -157,7 +158,7 @@ questTimeBox
 
 scoreBox <- ggplot(allData, aes(x = CONDITION, y = Score, fill = CONDITION)) +
   geom_boxplot() +
-  ggtitle("Sentence Conditions")
+  ggtitle("Sentence Conditions: Adversative ('Tuttavia')")
 scoreBox
 
 
@@ -307,3 +308,119 @@ write.xlsx(words_23,"C:/Users/herts/OneDrive/Desktop/words_23.xlsx",
 
 write.csv(words_22,"C:/Users/herts/OneDrive/Desktop/words_22.csv", 
           fileEncoding = "UTF-8")
+
+
+
+##############################################
+# Concessive Data
+##############################################
+
+italianConcess_Data <- read.csv("italianConcess_Data.csv")
+italianConds <- read.csv("italianConditions.csv", encoding = "Italian")
+italianConds <- italianConds[,c(-2:-4)]
+View(italianConcess_Data)
+
+
+italianConcData1a <- italianConcess_Data[c(1:5), c(2,4,5,7,8:87,636)]
+italianConcData1b <- italianConcess_Data[c(6:9), c(2,4,5,7,88:164,636)]
+italianConcData2a <- italianConcess_Data[c(10:15), c(2,4,5,7,165:244,636)]
+italianConcData2b <- italianConcess_Data[c(16:21), c(2,4,5,7,245:321,636)]
+italianConcData3a <- italianConcess_Data[c(22:26), c(2,4,5,7,322:401,636)]
+italianConcData3b <- italianConcess_Data[c(27:32), c(2,4,5,7,402:478,636)]
+italianConcData4a <- italianConcess_Data[c(33:35), c(2,4,5,7,479:558,636)]
+italianConcData4b <- italianConcess_Data[c(36:40), c(2,4,5,7,559:635,636)]
+View(italian)
+# Melt data to long format
+
+italianConcData1a <- reshape2::melt(italianConcData1a, id.vars = c(1:4,85))
+colnames(italianConcData1a)[colnames(italianConcData1a)=="variable"] = "Item"
+colnames(italianConcData1a)[colnames(italianConcData1a)=="value"] = "Score"
+
+italianConcData1b <- reshape2::melt(italianConcData1b, id.vars = c(1:4,82))
+colnames(italianConcData1b)[colnames(italianConcData1b)=="variable"] = "Item"
+colnames(italianConcData1b)[colnames(italianConcData1b)=="value"] = "Score"
+
+italianConcData2a <- reshape2::melt(italianConcData2a, id.vars = c(1:4,85))
+colnames(italianConcData2a)[colnames(italianConcData2a)=="variable"] = "Item"
+colnames(italianConcData2a)[colnames(italianConcData2a)=="value"] = "Score"
+
+italianConcData2b <- reshape2::melt(italianConcData2b, id.vars = c(1:4,82))
+colnames(italianConcData2b)[colnames(italianConcData2b)=="variable"] = "Item"
+colnames(italianConcData2b)[colnames(italianConcData2b)=="value"] = "Score"
+
+italianConcData3a <- reshape2::melt(italianConcData3a, id.vars = c(1:4,85))
+colnames(italianConcData3a)[colnames(italianConcData3a)=="variable"] = "Item"
+colnames(italianConcData3a)[colnames(italianConcData3a)=="value"] = "Score"
+
+italianConcData3b <- reshape2::melt(italianConcData3b, id.vars = c(1:4,82))
+colnames(italianConcData3b)[colnames(italianConcData3b)=="variable"] = "Item"
+colnames(italianConcData3b)[colnames(italianConcData3b)=="value"] = "Score"
+
+italianConcData4a <- reshape2::melt(italianConcData4a, id.vars = c(1:4,85))
+colnames(italianConcData4a)[colnames(italianConcData4a)=="variable"] = "Item"
+colnames(italianConcData4a)[colnames(italianConcData4a)=="value"] = "Score"
+
+italianConcData4b <- reshape2::melt(italianConcData4b, id.vars = c(1:4,82))
+colnames(italianConcData4b)[colnames(italianConcData4b)=="variable"] = "Item"
+colnames(italianConcData4b)[colnames(italianConcData4b)=="value"] = "Score"
+
+View(italianConcData1a)
+
+# Rbind and merge all data 
+
+allConcData <- rbind(italianConcData1a,italianConcData1b,italianConcData2a,italianConcData2b,
+                 italianConcData3a,italianConcData3b,italianConcData4a,italianConcData4b)
+
+allConcData <- merge(allConcData,italianConds, by.x = "Item", by.y = "SOS_ID")
+View(allConcData)
+
+# Means by Sentence Condition
+
+concCondNorms <- aggregate(Score~CONDITION, data=allConcData,mean); 
+names(concCondNorms) = c("CONDITION","Mean") 
+
+concCondSD <- aggregate(Score~CONDITION, data=allConcData,sd); 
+names(concCondSD)= c("CONDITION","SD") 
+
+concCondNorms <- merge(concCondNorms,concCondSD, by = "CONDITION")
+View(concCondNorms)
+
+# Plot for distributions
+
+# Draw with black outline, white fill
+ggplot(allConcData, aes(x=Score)) +
+  geom_histogram(binwidth=.5, colour="black", fill="steelblue") +
+  geom_density(aes(x=Score)) +
+  geom_vline(aes(xintercept=mean(Score, na.rm=T)),   # Ignore NA values for mean
+             color="red", linetype="dashed", size=1) +
+  ggtitle("Distribution: Concessive ('Nonostante cio')")
+
+# Density curve
+ggplot(allConcData, aes(x=Score)) + geom_density()
+
+timeBox <- ggplot(allConcData, aes(x = Quest, y = Time, fill = Quest)) +
+  geom_boxplot() +
+  ggtitle("Time to complete questionnaire")
+timeBox
+
+#timeNorms <- aggregate(Time~Quest, data=italianData,mean); 
+#names(timeNorms) = c("Quest","Mean") 
+
+#write.csv(timeNorms,"C:/Users/herts/OneDrive/Desktop/timeNorms.csv", fileEncoding = "UTF-8")
+
+scoreBox <- ggplot(allConcData, aes(x = CONDITION, y = Score, fill = CONDITION)) +
+  geom_boxplot() +
+  ggtitle("Sentence Conditions: Concessive ('Nonostante cio')")
+scoreBox
+
+# Means by Sentence Condition
+concCondNorms <- aggregate(Score~CONDITION, data=allConcData,mean); 
+names(concCondNorms) = c("CONDITION","Mean") 
+
+concCondSD <- aggregate(Score~CONDITION, data=allConcData,sd); 
+names(concCondSD)= c("CONDITION","SD") 
+
+concCondNorms <- merge(concCondNorms,concCondSD, by = "CONDITION")
+#View(concCondNorms)
+
+
